811855119c5d2152e8c934976debdedea60a6e9e
Camilo Villamizar <camilocodes@gmail.com>
2017-06-19 16:04:50 -0300

    DSETS-3 Complete event migration

---
+++ custom/modules/dsets_anniversaries_migration/src/Plugin/migrate/source/Event.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\dsets_anniversaries_migration\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+
+/**
+ * Source plugin for address content.
+ *
+ * @MigrateSource(
+ *   id = "event"
+ * )
+ */
+class Event extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $query = $this->select('events', 'e')
+      ->fields('e', ['event_id', 'event', 'source', 'event_date', 'note']);
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'event_id'    => $this->t('Event ID'),
+      'event'       => $this->t('Event'),
+      'source'      => $this->t('Source'),
+      'event_date'  => $this->t('Event Date'),
+      'note'        => $this->t('Note'),
+    ];
+
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'event_id' => [
+        'type' => 'integer',
+        'alias' => 'e',
+      ],
+    ];
+  }
+
+}
