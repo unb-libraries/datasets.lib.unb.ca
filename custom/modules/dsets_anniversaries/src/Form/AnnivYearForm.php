02addbe55c8cab4f81ee0a31c34a790a7e59110e
Camilo Villamizar <camilocodes@gmail.com>
2017-07-24 16:19:16 -0300

    DSETS-17 Add new form module for anniversries reference year

---
+++ custom/modules/dsets_anniversaries/src/Form/AnnivYearForm.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\dsets_anniversaries\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Controller for Anniversaries reference year form.
+ */
+class AnnivYearForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'dsets_anniv_year_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['reference_year'] = [
+      '#type' => 'textfield',
+      '#title' => t('Year:'),
+      '#required' => TRUE,
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+  }
+
+}
