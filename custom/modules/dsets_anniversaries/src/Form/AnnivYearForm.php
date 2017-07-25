15a92b6c2cc130fb1e79e30fdec1ebe99c718a11
Camilo Villamizar <camilocodes@gmail.com>
2017-07-25 11:27:30 -0300

    DSETS-17 Build anniversaries view from input year

--- custom/modules/dsets_anniversaries/src/Form/AnnivYearForm.php
+++ custom/modules/dsets_anniversaries/src/Form/AnnivYearForm.php
@@ -4,6 +4,7 @@ namespace Drupal\dsets_anniversaries\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
  * Controller for Anniversaries reference year form.
@@ -21,10 +22,18 @@ class AnnivYearForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+
     $form['reference_year'] = [
       '#type' => 'textfield',
       '#title' => t('Year:'),
       '#required' => TRUE,
+    ];
+
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Go'),
+      '#button_type' => 'primary',
     ];
 
     return $form;
@@ -40,6 +49,16 @@ class AnnivYearForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    foreach ($form_state->getValues() as $key => $value) {
+      drupal_set_message($key . ': ' . $value);
+    }
+
+    $year = $form_state->getValue('reference_year');
+    kint($year);
+
+    $response = new RedirectResponse('/anniversaries/' . $year);
+    $response->send();
   }
 
 }
