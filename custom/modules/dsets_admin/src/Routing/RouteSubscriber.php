19f43a6e22909d49c7abf22596d7371030b49bb0
camilocodes <cvillami@unb.ca>
2022-07-04 15:47:46 -0300

    DSETS-48 Remove \Drupal global object call + fix access restrictions

--- custom/modules/dsets_admin/src/Routing/RouteSubscriber.php
+++ custom/modules/dsets_admin/src/Routing/RouteSubscriber.php
@@ -3,11 +3,10 @@
 namespace Drupal\dsets_admin\Routing;
 
 use Drupal\Core\Routing\RouteSubscriberBase;
-use Drupal\user\Entity\User;
 use Symfony\Component\Routing\RouteCollection;
 
 /**
- * Listens to the dynamic route events and restrict access to user.pass route.
+ * Listens to the dynamic route events and restrict access to routes.
  */
 class RouteSubscriber extends RouteSubscriberBase {
 
@@ -23,17 +22,10 @@ class RouteSubscriber extends RouteSubscriberBase {
       'entity.user.edit_form',
     ];
 
-    // Get current user account object.
-    $account = \Drupal::currentUser();
-
-    // Only restrict for non-admin users.
-    if (!$account->hasPermission('administer users')) {
-
-      // Deny access to non-admins.
-      foreach ($deny_routes as $deny_route) {
-        if ($route = $collection->get($deny_route)) {
-          $route->setRequirement('_access', 'FALSE');
-        }
+    // Deny access to non-admins.
+    foreach ($deny_routes as $deny_route) {
+      if ($route = $collection->get($deny_route)) {
+        $route->setRequirement('_permission', 'administer_users');
       }
     }
   }
